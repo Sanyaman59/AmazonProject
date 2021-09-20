8f22f96263f21a23b718644ec663b90eeb0dd9ad
Oleksandr Stepanov <oleksandr.stepanov.qa@gmail.com>
2021-09-20 17:34:22 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
@@ -11,7 +12,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        var path = Paths.get("Components/chromedriver/chromedriver.exe");
+        Path path = Paths.get("Components/chromedriver/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", path.toString());
         WebDriver driver = new ChromeDriver();
         List<Book> books;
